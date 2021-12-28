2c641b16a7266f9704b921c104a19eeb5d64827a
June <june.qualwebs@gmail.com>
2021-12-28 11:37:36 +0530

    user api integrated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import UserResult from "../components/users/UserResult"
+
 function Home() {
     return (
         <div>
-            Home
+           
+            <UserResult/>
+           
         </div>
     )
 }
