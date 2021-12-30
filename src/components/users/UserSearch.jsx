3439c5ab83cc37c7ac18c71edce9990cda818a0f
June <june.qualwebs@gmail.com>
2021-12-30 14:01:10 +0530

    user page and list repos

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,18 +1,22 @@
 import {useState,useContext} from 'react';
 import GithubContext from '../../context/github/GithubContext';
-
+import AlertContext from '../../context/alert/AlertContext';
+import { searchUsers } from '../../context/github/GithubActions';
 function UserSearch() {
     const [text,setText]= useState('');
-    const {users,searchUsers,clearUsers}=useContext(GithubContext);
+    const {users,dispatch}=useContext(GithubContext);
+    const {setAlert} = useContext(AlertContext);
 
     const handleChange =(e)=> setText(e.target.value)
     
-    const handleSubmit =(e)=>{
+    const handleSubmit =async (e)=>{
         e.preventDefault()
         if(text === ''){
-            alert('Please enter somthing');
+            setAlert('Please enter somthing','error');
         }else {
-            searchUsers(text);
+            dispatch('SET_LOADING')
+            const users= await searchUsers(text);
+            dispatch({type:'GET_USERS', payload:users});
             setText('');
         }
     }
@@ -38,7 +42,7 @@ function UserSearch() {
             </div>
             {users.length>0 && (
                 <div>
-                    <button onClick ={clearUsers} className="btn btn-ghost">
+                    <button onClick ={()=> dispatch({type:'CLEAR_USERS'})} className="btn btn-ghost">
                         Clear
                     </button>
                 </div>
---
+++ src/context/github/GithubActions.js
@@ -0,0 +1,72 @@
+import axios from 'axios';
+
+const GITHUB_URL= process.env.REACT_APP_GITHUB_URL
+const GITHUB_TOKEN= process.env.REACT_APP_GITHUB_TOKEN
+
+const github=axios.create({
+    baseURL:GITHUB_URL,
+    headers:{Authorization:`token ${GITHUB_TOKEN}`}
+})
+export const searchUsers = async (text)=>{
+    
+    const params = new URLSearchParams({
+        q:text
+    })
+    const res = await github.get(`/search/users?${params}`);
+    return res.data.items;
+    
+}
+//get users and repos
+export const getUserAndRepos =async (login)=>{
+    const [user,repos]= await Promise.all([
+        github.get(`users/${login}`),
+        github.get(`users/${login}/repos`)
+    ]);
+    console.log(user, repos);
+    return {user:user.data, repos:repos.data }
+}
+
+//get single user  
+export const getUser = async (login)=>{
+  
+    const params = new URLSearchParams({
+        sort:'created',
+        per_page:10,
+    })
+    const res=await fetch(`${GITHUB_URL}/users/${login}?${params}`,{
+        headers:{
+            Authorization:`token ${GITHUB_TOKEN}`
+        }
+    });
+    if(res.status===404){
+        window.location= './notfound'
+    }else {
+        const data= await res.json();
+        return data;
+        // dispatch({
+        //     type:'GET_USER',
+        //     payload: data
+        // })
+    }
+    
+}
+//get repos
+export const getUserRepos = async (login)=>{
+    
+    const res=await fetch(`${GITHUB_URL}/users/${login}/repos`,{
+        headers:{
+            Authorization:`token ${GITHUB_TOKEN}`
+        }
+    });
+    if(res.status===404){
+        window.location= './notfound'
+    }else {
+        const data= await res.json();
+        return data;
+        // dispatch({
+        //     type:'GET_REPOS',
+        //     payload: data
+        // })
+    }
+    
+}
